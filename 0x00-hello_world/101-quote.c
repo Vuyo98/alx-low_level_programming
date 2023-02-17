d12a3045254fd2a7f658df5c1c2dbebec1f69444
Vuyo98 <vuyolwethunof@gmail.com>
2023-02-17 14:15:15 -0800

    task 8 fix.hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2025-10-19\n, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2025-10-19\n, 59);
      	return (1);
 }
